9ae2408bf01a7aaf391189173bf237c751a33e5a
Oghenekparobo <classicsteph11@gmail.com>
2022-04-08 10:39:49 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './public/*.html'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        font1: ['Grape Nuts'],
+        font2: ['Rubik Moonrocks']
+      }
+    },
+  },
+  plugins: [],
+}
